e4f12d6dfa38175a880213e850714b99f6f70a78
Michael Easter <codetojoy@gmail.com>
2015-02-28 18:37:56 -0400

    better doc for Main

--- src/main/java/org/peidevs/waro/Main.java
+++ src/main/java/org/peidevs/waro/Main.java
@@ -8,21 +8,28 @@ import org.peidevs.waro.config.ConfigService;
 
 import java.util.List;
 
+// To configure players, numGames, etc, see org.peidevs.waro.config.Config
+// This is compile-time configuration
+
 public class Main {
     
     public static void main(String... args) {
+        // read configuration 
         ConfigService configService = new ConfigService();
         int numCards = configService.getNumCards();
         int numGames = configService.getNumGames();
         boolean isVerbose = configService.isVerbose();        
         List<Player> players = configService.getPlayers();
         
+        // play the games
         Tourney tourney = new Tourney(numCards, numGames, isVerbose);
         List<Player> newPlayers = tourney.apply(players);
 
+        // find the winner
         PlayerComparator comparator = new PlayerComparator(PlayerStats::getNumGamesWon);
         Player winner = newPlayers.stream().max(comparator).get();
         
+        // report the winner 
         // TODO: report on tie games
         System.out.println("------------------");
         System.out.println("WINNER : " + winner.getName());
