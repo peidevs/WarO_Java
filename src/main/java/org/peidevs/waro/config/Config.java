58c0ebf015446f4a2584e8cc434e3cc491a63679
Michael Easter <codetojoy@gmail.com>
2015-02-28 18:07:23 -0400

    remove magic strings

--- src/main/java/org/peidevs/waro/config/Config.java
+++ src/main/java/org/peidevs/waro/config/Config.java
@@ -10,23 +10,32 @@ import java.util.*;
 
 @Configuration
 public class Config {
+    // -----------------------------------                
+    // configure these as desired
     private static final int NUM_CARDS = 12;
     private static final int MAX_CARD = NUM_CARDS;
     private static final int NUM_GAMES = 3;
     private static final boolean IS_VERBOSE = true;
-    
-    @Bean public int numCards() { return NUM_CARDS; }    
-    @Bean public int numGames() { return NUM_GAMES; }    
-    @Bean public boolean isVerbose() { return IS_VERBOSE; }
-    
+        
     @Bean
     public List<Player> players() {
         List<Player> players = new ArrayList<>();
                         
+        // -----------------------------------                
+        // configure players as desired
         players.add(new Player("Beethoven", new NextCard(), MAX_CARD));
         players.add(new Player("Chopin", new NextCard(), MAX_CARD));
         players.add(new Player("Mozart", new NextCard(), MAX_CARD));
         
         return players;
-    }   
+    }
+
+    @Bean public int numCards() { return NUM_CARDS; }    
+    @Bean public int numGames() { return NUM_GAMES; }    
+    @Bean public boolean isVerbose() { return IS_VERBOSE; }       
+    
+    protected static final String BEAN_NUM_CARDS = "numCards";
+    protected static final String BEAN_NUM_GAMES = "numGames";
+    protected static final String BEAN_IS_VERBOSE = "isVerbose";
+    protected static final String BEAN_PLAYERS = "players";    
 }
--- src/main/java/org/peidevs/waro/config/ConfigService.java
+++ src/main/java/org/peidevs/waro/config/ConfigService.java
@@ -13,11 +13,11 @@ public class ConfigService {
         context = new AnnotationConfigApplicationContext(Config.class);        
     }
     
-    public int getNumCards() { return context.getBean("numCards", Integer.class); }    
-    public int getNumGames() { return context.getBean("numGames", Integer.class); }    
-    public boolean isVerbose() { return context.getBean("isVerbose", Boolean.class); }
+    public int getNumCards() { return context.getBean(Config.BEAN_NUM_CARDS, Integer.class); }    
+    public int getNumGames() { return context.getBean(Config.BEAN_NUM_GAMES, Integer.class); }    
+    public boolean isVerbose() { return context.getBean(Config.BEAN_IS_VERBOSE, Boolean.class); }
     
     public List<Player> getPlayers() {
-        return context.getBean("players", List.class);
+        return context.getBean(Config.BEAN_PLAYERS, List.class);
     }    
 }
